c75d95ee113dd6d8232203c4dbc9853ddd3d4c32
patricio86 <pbadillo86@hotmail.com>
2021-03-09 13:26:21 +0100

    Falta darle una vuelta y hacer la funcion entre cajas

--- Rubendia/src/Caja.java
+++ Rubendia/src/Caja.java
@@ -1,15 +1,16 @@
+import java.util.Scanner;
 
 public class Caja {
 
 	private String id, nombre;
 	private double saldo;
 	
-	public Caja(String id, double saldo) {
+	public Caja(String id, String nombre, double saldo) {
 		this.id = id;
 		this.saldo = saldo;
 	}
 	
-	public Caja(String nombre) {
+	public Caja(String nombre, double ingresar, double retirar) {
 		this.nombre = nombre;
 	}
 	
@@ -35,6 +36,30 @@ public class Caja {
 
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
+	}
+	
+	public double ingresar(Caja dinero[]) {
+		double saldoingresar;
+		System.out.println("Cantidad a pagar:_");
+		Scanner sc = new  Scanner(System.in);
+		saldoingresar = sc.nextDouble();
+		System.out.println("Elige una caja por la que quieras pasar:_ \n 1 \n 2 \n 3 ");
+		int numcaja = sc.nextInt();
+		double suma = saldoingresar + dinero[numcaja].getSaldo();
+		double totalcaja = dinero[numcaja].getSaldo() + suma;
+		return totalcaja;
+	}
+	
+	public double retirar(Caja dinero[]) {
+		double saldoretirar;
+		System.out.println("Cantidad a pagar:_");
+		Scanner sc = new  Scanner(System.in);
+		saldoretirar = sc.nextDouble();
+		System.out.println("Elige una caja por la que quieras pasar:_ \n 1 \n 2 \n 3 ");
+		int numcaja = sc.nextInt();
+		double quita = dinero[numcaja].getSaldo() - saldoretirar;
+		double totalcaja = dinero[numcaja].getSaldo() - quita;
+		return totalcaja;
 	}
 	
 	
--- Rubendia/src/Principal.java
+++ Rubendia/src/Principal.java
@@ -2,16 +2,25 @@ import java.util.*;
 
 public class Principal {
 	
-	static Scanner sc = new  Scanner(System.in);
+	 
 
 	public static void main(String[]args) {
 		
-		Caja dinero[] = {new  Caja("Caja1",0), new  Caja("Caja2",55), new  Caja("Caja3",20) };		
-		Caja Cliente = new Caja("Cliente");
-		Caja Ruben = new Caja("Ruben");
-		Caja Cajero = new Caja("Cajero");
-		
-		
+		Caja dinero[] = {new  Caja("Caja1","Cajero1",0), new  Caja("Caja2","Cajero2",55), new  Caja("Caja3","Cajero3",20) };		
+		Caja persona1 = new Caja("Cliente",0.0,0.0);
+		Caja persona2 = new Caja("Ruben",0.0,0.0);
+	
 		System.out.println("Situacion actual: " + dinero[0].getId() + " tiene " + dinero[0].getSaldo() + " €, " + dinero[1].getId() + " tiene " + dinero[1].getSaldo() + " €, " + dinero[2].getId() + " tiene " + dinero[2].getSaldo() + " €, ");
+		persona1.ingresar(dinero);
+		System.out.println(" El " + persona1.getNombre() + " pasar por " + dinero[0].getId() + " e ingresa 50€");
+		System.out.println(persona1.ingresar(dinero));
+		
+		persona2.retirar(dinero);
+		System.out.println(persona2.getNombre() + " pasar por " + dinero[0].getId() + " y retira 50€");
+		System.out.println(persona2.retirar(dinero));
+		
+		
 	}
+	
+	
 }
